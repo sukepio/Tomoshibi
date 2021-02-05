d26cedd00fa58d4190d3612c779c92c7841ab171
sukepio <naoto927sukena@gmail.com>
2021-02-05 02:19:54 +0000

    Create admin in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  first_name: '直人',
+  last_name: '助名',
+  first_name_kana: 'ナオト',
+  last_name_kana: 'スケナ',
+  date_of_birth: '1995-09-27',
+  phone_number: '00011112222',
+  login_id: 'admin123',
+  email: 'admin@sample.com',
+  password: '123456',
+  )
