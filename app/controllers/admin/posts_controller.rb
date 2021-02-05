ab7256cd2ba5809983ce0a13acac35846a0852e1
sukepio <naoto927sukena@gmail.com>
2021-02-05 14:09:54 +0000

    add admin/post model, controller

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,26 @@
+class Admin::PostsController < ApplicationController
+
+
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_admin.posts.new(post_params)
+
+    if @post.save
+      redirect_to admin_posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+end
