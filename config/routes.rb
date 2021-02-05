4ce3fa7bdeecafa797a7d309daf380013790ccf9
sukepio <naoto927sukena@gmail.com>
2021-02-05 02:35:31 +0000

    Add temporary header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  root 'homes#about'
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
