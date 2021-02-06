8fb53a5a953d5eb79c37d83994936e740c4b568e
sukepio <naoto927sukena@gmail.com>
2021-02-06 09:52:44 +0000

    enabled to post images to S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   process resize_to_limit: [400, 400]
   # Override the directory where uploaded files will be stored.
