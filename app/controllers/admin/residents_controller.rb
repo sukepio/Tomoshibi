3c45d0b0c77bfb3f2a816dea9ce2071bb45b7e2b
sukepio <naoto927sukena@gmail.com>
2021-02-07 12:39:17 +0000

    add admin/resident/edit view

--- app/controllers/admin/residents_controller.rb
+++ app/controllers/admin/residents_controller.rb
@@ -7,4 +7,23 @@ class Admin::ResidentsController < ApplicationController
   def show
     @resident = Resident.find(params[:id])
   end
+
+  def edit
+    @resident = Resident.find(params[:id])
+  end
+
+  def update
+    @resident = Resident.find(params[:id])
+    if @resident.update(resident_params)
+      redirect_to admin_resident_path(@resident), notice: "#{@resident.full_name}さんの情報を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def resident_params
+    params.require(:resident).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number, :login_id, :date_of_birth, :gender, :information)
+  end
 end
