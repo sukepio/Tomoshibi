5b4199e8f1194061715ffae638cde63645752515
sukepio <naoto927sukena@gmail.com>
2021-02-11 04:38:40 +0000

    finish admin-resident-search

--- app/controllers/admin/residents_controller.rb
+++ app/controllers/admin/residents_controller.rb
@@ -3,7 +3,7 @@ class Admin::ResidentsController < ApplicationController
   def index
     @q = Resident.ransack(params[:q])
     @households = Household.all
-    @residents = @q.result.includes(:household)
+    @residents = @q.result.includes(:household).page(params[:page]).per(10)
   end
 
   def show
