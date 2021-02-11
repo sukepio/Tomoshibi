d39e8bf4fb0d6e6694f35782a9f64fdbf247b53b
sukepio <naoto927sukena@gmail.com>
2021-02-11 06:01:25 +0000

    Add resident header

---
+++ test/controllers/public/residents_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class Public::ResidentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get public_residents_show_url
+    assert_response :success
+  end
+
+end
