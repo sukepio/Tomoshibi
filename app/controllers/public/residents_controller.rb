a7c36889675cb2b0864c16d57e23e5c59c4d1da8
sukepio <naoto927sukena@gmail.com>
2021-02-11 23:50:00 +0000

    add translation of myevent to ja.yaml

--- app/controllers/public/residents_controller.rb
+++ app/controllers/public/residents_controller.rb
@@ -1,5 +1,6 @@
 class Public::ResidentsController < ApplicationController
   def show
     @myevents = current_resident.myevents
+    @today_events = @myevents.where(start: Time.now.in_time_zone("Tokyo").all_day)
   end
 end
