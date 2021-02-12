b49585603adfa15b16b6b29593faa85bc513fc88
sukepio <naoto927sukena@gmail.com>
2021-02-12 09:34:25 +0000

    make postbody wrap

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < ApplicationController
 
 
   def index
-    @posts = Post.page(params[:page])
+    @posts = Post.page(params[:page]).order(created_at: :desc)
   end
 
   def show
