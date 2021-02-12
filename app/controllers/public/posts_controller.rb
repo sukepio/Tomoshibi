9680fc6e5b5f93ac4996f52e8261dc8a1acb2f7d
sukepio <naoto927sukena@gmail.com>
2021-02-12 07:12:00 +0000

    add resident-post index

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,8 @@
 class Public::PostsController < ApplicationController
+
   def index
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true).page(params[:page]).per(10)
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 20 }
 
+
+  def time_format
+    wd = ["日", "月", "火", "水", "木", "金", "土"]
+    updated_at.strftime("%-m/%-d(#{wd[updated_at.wday]})")
+  end
+
 end
