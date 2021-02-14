f56c0ef11b7b6835bb43e9adb311d59d91a74508
sukepio <naoto927sukena@gmail.com>
2021-02-14 05:29:30 +0000

    make bookmark asynchronous

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -8,15 +8,15 @@ class Public::BookmarksController < ApplicationController
   def create
     @bookmark = current_resident.bookmarks.new(post_id: @post.id)
     @bookmark.save
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
   end
 
   def destroy
     @bookmark = current_resident.bookmarks.find_by(post_id: @post.id)
     @bookmark.destroy
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
   end
-  
+
   private
 
   def set_post
