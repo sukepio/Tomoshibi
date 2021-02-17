09b511704f01992b882475a09f90f6e6c5bf2fac
sukepio <naoto927sukena@gmail.com>
2021-02-17 07:05:10 +0000

    finish admin-post-index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,7 +3,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @posts = Post.page(params[:page]).order(created_at: :desc)
+    @posts = Post.page(params[:page]).includes([:admin]).order(created_at: :desc)
   end
 
   def show
