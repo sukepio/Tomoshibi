4a63bffdc2696bc207a0f6f67ed43680346a990b
sukepio <naoto927sukena@gmail.com>
2021-02-18 01:38:28 +0000

    fix active storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'tomoshibi-post-image'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tomoshibi-post-image'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
