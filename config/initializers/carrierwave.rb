7cb2253730c2e839effe6d7cb1f9f5936cc31fef
sukepio <naoto927sukena@gmail.com>
2021-02-18 02:25:20 +0000

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,11 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'tomoshibi-post-image'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tomoshibi-post-image'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
-      region: 'ap-northeast-1', 
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
       path_style: true
     }
-end 
+end
