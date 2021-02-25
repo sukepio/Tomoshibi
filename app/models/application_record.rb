4f801d88a286615527e24e94222e8a9dc269397b
sukepio <naoto927sukena@gmail.com>
2021-02-25 09:46:37 +0000

    fix css

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def time_format
+  def time
     start.strftime("%H:%M")
   end
 
-  def time
+  def time_format
     wd = ["日", "月", "火", "水", "木", "金", "土"]
     created_at.strftime("%-m/%-d(#{wd[created_at.wday]})")
   end
