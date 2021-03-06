4cd69c110f6a9b0b1c36b6ad4bb0fd8acf13a448
sukepio <naoto927sukena@gmail.com>
2021-03-06 03:20:51 +0000

    apply image-change to resident-post-show

--- app/assets/javascripts/admin/post.js
+++
@@ -1,9 +0,0 @@
-//イメージの切り替え
-$(function(){   
-  const images = document.querySelectorAll('.thumb');
-  images.forEach(function(image, index){
-    image.onclick = function(){
-      document.getElementById('bigimg').src = this.src;
-    }
-  });
-});
--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -14,3 +14,13 @@ $(function() {
    });
   });
 });
+
+//イメージの切り替え
+$(function(){   
+  const images = document.querySelectorAll('.thumb');
+  images.forEach(function(image, index){
+    image.onclick = function(){
+      document.getElementById('bigimg').src = this.src;
+    }
+  });
+});
